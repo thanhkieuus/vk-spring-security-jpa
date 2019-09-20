4665d447a4b2d03588629795b886b50446adbe2a
thanhkieuus <voltakieu@yahoo.com>
2019-09-19 21:10:48 -0500

    First commit

---
+++ vk-spring-security-jpa/src/main/java/com/vk/springsecurity/VkSpringSecurityJpaApplication.java
@@ -0,0 +1,13 @@
+package com.vk.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkSpringSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkSpringSecurityJpaApplication.class, args);
+	}
+
+}
---
+++ vk-spring-security-jpa/src/test/java/com/vk/springsecurity/VkSpringSecurityJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vk.springsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VkSpringSecurityJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
